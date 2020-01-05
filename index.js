3427c3790a2ace2a7043036b1fea4f583d6affc8
aKan Chen <ken@astrocorp.com.tw>
2020-01-05 21:18:23 +0800

    add assets/spineboy

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-$(document).ready(function () {
-    
+window.onload = function () {
+    var app = new PIXI.Application();
+
+    document.body.appendChild(app.view);
+
+    // load spine data
+    app.loader
+        .add('spineboy', 'assets/spineboy-pro.json')
+        .load(onAssetsLoaded);
+
+    app.stage.interactive = true;
+
+    function onAssetsLoaded(loader, res) {
+        // create a spine boy
+        var spineboy = new PIXI.spine.Spine(res.spineboy.spineData);
+
+        app.stage.addChild(spineboy);
+
+        // set the position
+        spineboy.x = app.screen.width / 2;
+        spineboy.y = app.screen.height;
+
+        spineboy.scale.set(0.5);
+
+        // set current skin
+        spineboy.state.setAnimation(0, 'run', true);
+    }
 }
